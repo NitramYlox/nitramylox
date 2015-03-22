c19a77a1d5a5057540d90a238f45bcd66d988eea
NitramYlox <nitramylox@gmail.com>
2015-03-22 17:22:29 +0100

    First try
    Im only trying what i am doing

---
+++ config/defaults.js
@@ -0,0 +1,36 @@
+define({
+    "appid": "",
+    "webmap": "8dd583ea3de64e40b92ea5a261d0c6c8",
+    "oauthappid": null,
+    "proxyurl": "",
+    "bingmapskey": null,
+    "sharinghost": location.protocol + "//" + "www.arcgis.com",
+    "scalebarUnits": "dual",
+    //Set config properties for all of these 
+    "geocoder": false,  //false in config values 
+    "searchExtent": true,
+    "searchLayers":[{
+        "id": "",
+        "fields": []
+    }],
+    "locationSearch": false,  
+    "titleInHeader": true,  //true in config values 
+    "displaypopups": true, //true in config values
+    "showHelpOnLoad": true, //true in config values 
+    //end set config 
+    "helperServices": {
+    "geometry": {
+      "url": null
+    },
+    "printTask": {
+      "url": null
+    },
+    "elevationSync": {
+      "url": null
+    },
+    "geocode": [{
+      "url": null
+        }]
+  }
+});
+
---
+++ js/nls/et/strings.js
@@ -0,0 +1,16 @@
+﻿define(
+   ({
+    map: {
+      error: "Kaardi loomine ebaõnnestus"
+    }, 
+    tools: {
+      search: {
+          "error": "Asukohta ei leitud",
+          "notWhatYouWanted": "Ei ole see, mida soovisid?",
+          "selectAnother": "Vali muu asukoht",
+          "currentLocation": "Praegune asukoht",
+          "title": "Asukoht"
+      }
+    }
+  })
+);
